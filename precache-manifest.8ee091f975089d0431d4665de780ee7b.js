df212864ca382185ac5742df2f9ee6214c4c03da
Gleb Irovich <ch.cookiehunter@gmail.com>
2019-04-19 19:59:27 +0200

    Updates

---
+++ precache-manifest.8ee091f975089d0431d4665de780ee7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e95c9f5bef18a793526288b4f28635bb",
+    "url": "/the-bachelor-game/static/media/dummy.e95c9f5b.svg"
+  },
+  {
+    "revision": "4ce4d7e9e23d06ebfcb7",
+    "url": "/the-bachelor-game/static/js/runtime~main.eb66bfe3.js"
+  },
+  {
+    "revision": "c8da476d5ff2fce7a070",
+    "url": "/the-bachelor-game/static/js/main.4da53dc3.chunk.js"
+  },
+  {
+    "revision": "cc70745286a77643754d",
+    "url": "/the-bachelor-game/static/js/2.9ea3ccc6.chunk.js"
+  },
+  {
+    "revision": "c8da476d5ff2fce7a070",
+    "url": "/the-bachelor-game/static/css/main.0f6d2f58.chunk.css"
+  },
+  {
+    "revision": "84b6e8c36b40a5d84a7e211f49568933",
+    "url": "/the-bachelor-game/index.html"
+  }
+];
