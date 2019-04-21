02bd25df1f84244abbd4bd74fe62827c1b07e3d0
Gleb Irovich <ch.cookiehunter@gmail.com>
2019-04-21 19:07:23 +0200

    Updates

---
+++ precache-manifest.4518ae873c7ef9b4164e11219b7e069f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e95c9f5bef18a793526288b4f28635bb",
+    "url": "/the-bachelor-game/static/media/dummy.e95c9f5b.svg"
+  },
+  {
+    "revision": "4ce4d7e9e23d06ebfcb7",
+    "url": "/the-bachelor-game/static/js/runtime~main.eb66bfe3.js"
+  },
+  {
+    "revision": "8731557045c4cdc38642",
+    "url": "/the-bachelor-game/static/js/main.069456cf.chunk.js"
+  },
+  {
+    "revision": "ef1dca555ef88f079fa1",
+    "url": "/the-bachelor-game/static/js/2.1b5e6ab0.chunk.js"
+  },
+  {
+    "revision": "8731557045c4cdc38642",
+    "url": "/the-bachelor-game/static/css/main.9f13f85a.chunk.css"
+  },
+  {
+    "revision": "f6f7e708ff2970acb0ae61ed1d6a83bc",
+    "url": "/the-bachelor-game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/the-bachelor-game/precache-manifest.b6245223dab57ddd58e179445ae1618d.js"
+  "/the-bachelor-game/precache-manifest.4518ae873c7ef9b4164e11219b7e069f.js"
 );
 
 workbox.clientsClaim();
